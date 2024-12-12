db4717129be6579237247e8ea802cac48667fef3
Pratyush Sharma <123284482+pratyush605@users.noreply.github.com>
2024-12-12 23:18:17 +0530

    Delete Conceptile directory

--- Conceptile/src/main/java/Pratyush/Conceptile/Repo/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package Pratyush.Conceptile.Repo;
-
-import Pratyush.Conceptile.Model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepo extends JpaRepository<Quiz, Integer> {
-}
